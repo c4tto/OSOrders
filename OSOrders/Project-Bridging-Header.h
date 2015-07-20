435045bf431dd158f6dcb1959307ec2c106625ca
Ondrej Stocek <ondrej.stocek@strv.com>
2015-07-20 11:47:49 +0200

    added basic stuff

---
+++ OSOrders/Project-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Project-Bridging-Header.h
+//  OSOrders
+//
+//  Created by Ondřej Štoček on 19.07.15.
+//  Copyright (c) 2015 Ondrej Stocek. All rights reserved.
+//
+
+#ifndef OSOrders_Project_Bridging_Header_h
+#define OSOrders_Project_Bridging_Header_h
+
+#import <AFNetworking/AFNetworking.h>
+
+#endif
