4442f19a5484176c6c7893840e10988af802e0c0
Ondrej Stocek <ondrej.stocek@strv.com>
2015-12-08 10:52:48 +0100

    coding style

--- OSOrders/ErrorType+ServerApi.swift
+++ OSOrders/ErrorType+ServerApi.swift
@@ -13,18 +13,15 @@ import SwiftyJSON
 extension ErrorType {
     
     var serverApiError: ErrorType? {
-        let jsonData = ((self as Any) as! NSError).userInfo[AFNetworkingOperationFailingURLResponseDataErrorKey] as? NSData
-        
-        if let jsonData = jsonData {
+		if let jsonData = ((self as Any) as! NSError).userInfo[AFNetworkingOperationFailingURLResponseDataErrorKey] as? NSData {
             let json = JSON(data: jsonData)
-            
+			
             if let code = json["error"]["code"].int, message = json["error"]["message"].string {
                 return NSError(domain: "OrdersServerErrorDomain", code: code, userInfo: [
                     NSLocalizedDescriptionKey: message
                 ])
             }
         }
-        
         return nil
     }
 }
